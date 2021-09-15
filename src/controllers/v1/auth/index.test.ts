b3818aa43ed51339546585f09570f89bdbdc674a
vinay.mavi <vinaymavi@gmail.com>
2021-09-15 23:19:47 +0530

    Unit tests updated

--- src/controllers/v1/auth/index.test.ts
+++ src/controllers/v1/auth/index.test.ts
@@ -1,14 +1,4 @@
-import mockingoose from "mockingoose";
-import httpMock from "node-mocks-http";
-import { UserModel } from "../../../models/";
 import { AuthController } from "./index";
-const _doc = {
-  _id: "507f191e810c19729de860ea",
-  name: "name",
-  email: "name@email.com",
-  password: "$2b$10$WYM5ghm/iZO0c5PQJDDQVO2XsUPCU91CzHHAdDFPSngCf1MdJvhAe",
-  permissions: ["app.user.admin"],
-};
 
 describe("Auth Controller", () => {
   let ctrl: AuthController;
--- src/helpers/user.helper.test.ts
+++ src/helpers/user.helper.test.ts
@@ -1,17 +1,4 @@
-import mockingoose from "mockingoose";
-
-import User from "../models/user.model";
 import { UserHelper } from "./user.helper";
-
-const _doc = {
-  _id: "507f191e810c19729de860ea",
-  name: "name",
-  email: "name@email.com",
-  password: "$2b$10$WYM5ghm/iZO0c5PQJDDQVO2XsUPCU91CzHHAdDFPSngCf1MdJvhAe",
-  permissions: ["app.user.admin"],
-};
-
-mockingoose(User).toReturn(_doc, "findOne");
 
 describe("User Helper Tests", () => {
   it("Module should loaded", () => {
